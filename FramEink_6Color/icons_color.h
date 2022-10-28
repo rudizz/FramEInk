8340677c68aa623156f26f81996bf225ebe81d32
rudizz <53229841+rudizz@users.noreply.github.com>
2022-10-28 15:05:22 +0200

    caricato nuove icone color weather

---
+++ FramEink_6Color/icons_color.h
@@ -0,0 +1,18 @@
+#ifndef ICONS_H
+#define ICONS_H
+
+#include "binary_Icons/color_01d_clear.h"
+#include "binary_Icons/color_02d_few_clouds.h"
+#include "binary_Icons/color_03d_scattered_clouds.h"
+#include "binary_Icons/color_04d_broken_clouds.h"
+#include "binary_Icons/color_09d_shower_rain.h"
+#include "binary_Icons/color_10d_rain.h"
+#include "binary_Icons/color_11d_thunderstorm.h"
+#include "binary_Icons/color_13d_snow.h"
+#include "binary_Icons/color_50d_mist.h"
+
+#include "binary_Icons/autumn.h"
+#include "binary_Icons/celsius_70.h"
+#include "binary_Icons/termometro.h"
+
+#endif
